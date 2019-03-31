205e24e0dfcef6cb6cdfe9d6a119e17eecdbfba8
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-03-31 22:26:50 +0900

    Refactor dynamo

--- src/lib/DynamoDB.ts
+++ src/lib/DynamoDB.ts
@@ -1,14 +1,14 @@
-import * as AWS from 'aws-sdk';
+import { DynamoDB as AwsDynamoDB } from 'aws-sdk';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 export default class DynamoDB {
   static client(isOffline: boolean = false): DocumentClient {
     if (isOffline) {
-      return new AWS.DynamoDB.DocumentClient({
+      return new AwsDynamoDB.DocumentClient({
         region: 'localhost',
         endpoint: 'http://localhost:8000'
       });
     }
-    return new AWS.DynamoDB.DocumentClient();
+    return new AwsDynamoDB.DocumentClient();
   }
 }
