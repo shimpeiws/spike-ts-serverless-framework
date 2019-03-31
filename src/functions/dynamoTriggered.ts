0ca93a5bdf2f2fd384763bc403c8c7f34400cae5
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-03-31 22:56:46 +0900

    Implement dynamoTriggered

--- src/functions/dynamoTriggered.ts
+++ src/functions/dynamoTriggered.ts
@@ -1,11 +1,37 @@
 import { Callback, Context, Handler } from 'aws-lambda';
 import { ServerlessAPIGatewayEvent } from 'aws-lambda';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { ApiGatewayManagementApi } from 'aws-sdk';
+import DynamoDB from '../lib/DynamoDB';
+import ApiGateway from '../lib/ApiGateway';
 
 export const index: Handler = async (
   event: ServerlessAPIGatewayEvent,
   _context: Context,
   callback: Callback
 ) => {
+  const apiGateway = ApiGateway.client();
+  const dynamo = DynamoDB.client(event.isOffline);
+  const scanParams: DocumentClient.ScanInput = {
+    TableName: process.env.CONNECTIONS_DYNAMODB_TABLE,
+    ProjectionExpression: 'ConnectionId'
+  };
+  const socketClients = await dynamo.scan(scanParams).promise();
+  socketClients.Items.map(async ({ ConnectionId }) => {
+    const imageParams: DocumentClient.ScanInput = {
+      TableName: process.env.IMAGES_DYNAMODB_TABLE
+    };
+    const images = await dynamo.scan(imageParams).promise();
+    let resObj = {};
+    images.Items.map(item => {
+      resObj[item.query] = item.urls;
+    });
+    const request: ApiGatewayManagementApi.PostToConnectionRequest = {
+      ConnectionId: ConnectionId,
+      Data: JSON.stringify(resObj)
+    };
+    await apiGateway.postToConnection(request).promise();
+  });
   const response = {
     statusCode: 200,
     body: JSON.stringify({
